d4d5951cb8f6f88055e908acf9d8fc6635fddc61
remotezen <remotezen@bell.net>
2012-12-24 19:27:57 -0500

    checking commit status

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ group :development, :test do
   #gem 'sqlite3', '1.3.5'
   gem  'pg', '0.12.2'
   gem 'rspec-rails', '2.11.0'
+  gem 'guard-rspec', '1.2.1'
+  gem 'guard-spork', '1.2.0'
+  gem 'spork', '0.9.2'
 end
 
 
@@ -29,6 +32,9 @@ group :production do
 end
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-inotify','0.8.8'
+  gem 'libnotify', '0.5.9'
+
 end
 
 # To use ActiveModel has_secure_password
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,25 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title){ "Heroku App | " }
   describe "GET /static_pages" do
     describe "Home page"  do
+  
       it "should have the content Heroku app" do 
         visit '/static_pages/home'
         page.should have_content('Heroku App')
       end
+      it "should have h3 content home" do
+        visit '/static_pages/home'
+        page.should have_selector('h3',
+                                  text: "Home"
+                                 )
+
+      end
       it "should have the right title" do
         visit '/static_pages/home'
         page.should have_selector('title',
-                                 text: "Heroku App | Home")
+                                 text: "#{base_title}Home")
       end
 
     end
@@ -18,13 +27,17 @@ describe "StaticPages" do
       it "should have the Help page for Heroku app" do
         visit '/static_pages/help'
         page.should have_selector('title',
-                                 text: "Heroku App | Help")
+
+                                 text: "#{base_title}Help"
+                                 )
       end
     end
     describe "About page" do
       it "should have the content 'About Us' " do
         visit '/static_pages/about'
-        page.should have_selector('title', text:"Heroku App | About Us")
+        page.should have_selector('title', 
+                                 text: "#{base_title}About Us"
+                                 )
       end
     end
   end
